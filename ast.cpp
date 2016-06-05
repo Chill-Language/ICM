aa8273931ed0923d9d0060f5cfe9e4827a7d280e
Chill Magic <hanlengmowang@hotmail.com>
2016-06-05 19:28:27 +0800

    Update ast.cpp

--- ast.cpp
+++ ast.cpp
@@ -12,6 +12,91 @@ using std::string;
 
 namespace ICM
 {
+	// Method
+	// ObjectData
+	template <typename T>
+	explicit ObjectData::ObjectData(const T &data) {
+		setData(data);
+	}
+	template <typename T>
+	void ObjectData::release() {
+		getPointer<T>()->~T();
+		pointer = nullptr;
+		size = 0;
+	}
+	ObjectData* ObjectData::clone() const {
+		ObjectData *cpy = new ObjectData(*this);
+		if (this->size) {
+			cpy->pointer = memcpy((char*)malloc(this->size), this->pointer, this->size);
+		}
+		return cpy;
+	}
+	template <typename T>
+	void ObjectData::setData(const T & data) {
+		if (!pointer) {
+			pointer = new T(data);
+			size = sizeof(T);
+		}
+		else {
+			*getPointer<T>() = data;
+		}
+	}
+	// ASTNode
+	void ASTNode::initialize(ASTNodeType type) {
+		this->reference = false;
+		switch (type) {
+		case AST_DATA:
+			initialize(new ObjectData());
+			break;
+		case AST_FUNC:
+			initialize(new Function(), new Parameters());
+			break;
+		default:
+			this->type = type;
+		}
+	}
+	void ASTNode::initialize(ObjectData *dat) {
+		this->type = AST_DATA;
+		this->objdata = dat;
+	}
+	void ASTNode::initialize(Function *fun = nullptr, Parameters *par = nullptr) {
+		this->type = AST_FUNC;
+		this->fundata.func = fun;
+		this->fundata.pars = par;
+	}
+	void ASTNode::release() {
+		if (reference)
+			return;
+		switch (type) {
+		case AST_DATA:
+			delete this->objdata;
+			break;
+		case AST_FUNC:
+			delete this->fundata.func;
+			delete this->fundata.pars;
+			break;
+		default:
+			;
+		}
+	}
+	ASTNode* ASTNode::clone() const {
+		ASTNode *copy = new ASTNode();
+		copy->type = this->type;
+		switch (this->type) {
+		case AST_DATA:
+			copy->objdata = this->objdata->clone();
+			break;
+		case AST_FUNC:
+			copy->fundata.func = this->fundata.func; // TODO
+			copy->fundata.pars = this->fundata.pars; // TODO
+			break;
+		default:
+			;
+		}
+		return copy;
+	}
+
+	// ToString
 	string to_string(const ObjectData *obj) {
 		if (obj->pointer == nullptr)
 			return "Null";
