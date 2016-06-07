53558f160a34c030cc7e1fb979ebacee57eb1974
Chill Magic <hanlengmowang@hotmail.com>
2016-06-07 10:42:04 +0800

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,6 @@
 #include "prints.h"
 #include "ast.h"
-#include <stack>
-using std::string;
-using std::vector;
-using std::stack;
+#include "match.h"
 using namespace System;
 
 template <typename T>
@@ -23,13 +20,38 @@ int main(void)
 {
 	AST *ast;
 
+	const char *text = "(+ 5 6 ); sss";
+	Match match(text);
+
+	ast = new AST();
+	char *begin = nullptr;
+
+	MatchResult mr;
+	println(match.matchNext());
+	println(match.matchNext());
+	println(match.matchNext());
+	println(match.matchNext());
+	println(match.matchNext());
+	/*while (mr.begin() != mr.end()) {
+		println(mr);
+		if (mr.getType() == T_LBracket)
+			ast->pushNode(AST_FUNC);
+		if (mr.getType() == T_RBracket)
+			ast->retNode();
+		if (mr.getType() == T_Identifier)
+			ast->setfunc(FUNC_DEF, mr.getAddMsg());
+		match.matchNext();
+	}*/
+	println(ast);
+
+	/*
 	// (+ 5 6)
 	ast = new AST();
 	
-	ast->pushNode(AST_FUNC)->setfunc(FUNC_DEF, 1);
-	ast->pushNode(AST_DATA)->setdata(5);
-	ast->pushNode(AST_DATA)->setdata(6);
-	ast->retNode();
+	ast->pushNode(AST_FUNC)->setfunc(FUNC_DEF, 1); // ( +
+	ast->pushNode(AST_DATA)->setdata(5);           // 5
+	ast->pushNode(AST_DATA)->setdata(6);           // 6
+	ast->retNode();                                // )
 
 	println(ast);
 	delete ast;
@@ -37,16 +59,17 @@ int main(void)
 	// (+ 7 (+ 5 6))
 	ast = new AST();
 
-	ast->pushNode(AST_FUNC)->setfunc(FUNC_DEF, 1);
-	ast->pushNode(AST_DATA)->setdata(7);
-	ast->pushNode(AST_FUNC)->setfunc(FUNC_DEF, 1);
-	ast->pushNode(AST_DATA)->setdata(5);
-	ast->pushNode(AST_DATA)->setdata(6);
-	ast->retNode();
-	ast->retNode();
+	ast->pushNode(AST_FUNC)->setfunc(FUNC_DEF, 1); // ( +
+	ast->pushNode(AST_DATA)->setdata(7);           // 7
+	ast->pushNode(AST_FUNC)->setfunc(FUNC_DEF, 1); // ( +
+	ast->pushNode(AST_DATA)->setdata(5);           // 5
+	ast->pushNode(AST_DATA)->setdata(6);           // 6
+	ast->retNode();                                // )
+	ast->retNode();                                // )
 
 	println(ast);
 	delete ast;
+	*/
 
 	return 0;
 }
